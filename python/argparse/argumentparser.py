98862de5ca9e77fc108e00b79dffe8c72ce24c0a
narupo <narupo.glab@gmail.com>
2021-04-03 02:23:16 +0900

    ok

--- python/argparse/argumentparser.py
+++ python/argparse/argumentparser.py
@@ -3,3 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser('mycmd', description='これはプログラムの説明です')
 parser.print_help()
+# 
+# usage: mycmd [-h]
+# 
+# これはプログラムの説明です
+# 
+# optional arguments:
+#   -h, --help  show this help message and exit
