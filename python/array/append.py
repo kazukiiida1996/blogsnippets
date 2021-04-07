fc4f2cb171ef39a04f81475e17aa1cffae2c3325
narupo <narupo.glab@gmail.com>
2021-04-08 02:29:21 +0900

    ok

--- python/array/append.py
+++ python/array/append.py
@@ -9,12 +9,3 @@ arr.append(3)
 
 print(arr)
 # array('i', [1, 2, 3])
-
-
-arr = array.array('B')
-
-try:
-    arr.append(-1)
-except OverflowError as e:
-    print(e)
-    # unsigned byte integer is less than minimum
