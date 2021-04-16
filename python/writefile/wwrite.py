c05edc166fd458ed57d12c3b2b1c895e3aff250d
narupo <narupo.glab@gmail.com>
2021-04-16 13:27:13 +0900

    fix

--- python/writefile/wwrite.py
+++ python/writefile/wwrite.py
@@ -1,4 +1,3 @@
 with open('file.txt', 'w') as fout:
     fout.write('本日は晴天なり。')
-    fout.write('いい天気ですね。')
     # 本日は晴天なり。いい天気ですね。
---
+++ python/writefile/wwrited.py
@@ -0,0 +1,4 @@
+with open('file.txt', 'w') as fout:
+    fout.write('本日は晴天なり。')
+    fout.write('いい天気ですね。')
+    # 本日は晴天なり。いい天気ですね。
