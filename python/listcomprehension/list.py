0c1bd0a24ed204a2327053ea3fe347a9f8fe2843
narupo <narupo.glab@gmail.com>
2021-05-22 01:13:08 +0900

    ok

--- python/listcomprehension/list.py
+++ python/listcomprehension/list.py
@@ -31,3 +31,23 @@ print(lis)
 matrix = [[i for i in range(4)] for i in range(2)]
 print(matrix)
 # [[0, 1, 2, 3], [0, 1, 2, 3]]
+
+
+lis1 = ['cat', 'dog', 'bird', 'pig']
+lis2 = [0, 1, 2, 3]
+lis3 = [kv for kv in zip(lis1, lis2)]
+print(lis3)
+# [('cat', 0), ('dog', 1), ('bird', 2), ('pig', 3)]
+
+
+lis1 = ['cat', 'dog', 'bird', 'pig']
+lis2 = [0, 1, 2, 3]
+lis3 = [(v, k) for k, v in zip(lis1, lis2)]
+print(lis3)
+# [(0, 'cat'), (1, 'dog'), (2, 'bird'), (3, 'pig')]
+
+
+lis1 = ['cat', 'dog', 'bird', 'pig']
+lis2 = [(i, v) for i, v in enumerate(lis1)]
+print(lis2)
+# [(0, 'cat'), (1, 'dog'), (2, 'bird'), (3, 'pig')]
