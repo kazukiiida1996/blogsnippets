85bcc5aea1ba8e56b223fbe57ad080ab6564fb75
narupo <narupo.glab@gmail.com>
2021-05-28 00:31:31 +0900

    ok

--- python/twodimarray/telescopic.py
+++ python/twodimarray/telescopic.py
@@ -14,7 +14,29 @@ twoarr = [
     [3, 4, 5],
 ]
 
+twoarr[0].append(6)
+
+print(twoarr)
+# [[0, 1, 2, 6], [3, 4, 5]]
+
+
+twoarr = [
+    [0, 1, 2],
+    [3, 4, 5],
+]
+
 twoarr.insert(0, [6, 7, 8])
 
 print(twoarr)
 # [[6, 7, 8], [0, 1, 2], [3, 4, 5]]
+
+
+twoarr = [
+    [0, 1, 2],
+    [3, 4, 5],
+]
+
+twoarr[1].insert(0, 6)
+
+print(twoarr)
+# [[0, 1, 2], [6, 3, 4, 5]]
