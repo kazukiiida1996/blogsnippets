2e58f8bbf2f1babdf26dcf3c0fed72c7d179749b
narupo <narupo.glab@gmail.com>
2021-06-02 03:55:02 +0900

    ok

--- python/strfind/find.py
+++ python/strfind/find.py
@@ -8,13 +8,13 @@ index = s.find('Dog')
 print(index)  # -1
 
 
+s = 'Cat and Cat'
+index = s.find('Cat', 3)
+print(index)  # 8
+
+
 s = 'The Cat'
 try:
     s.find(None)
 except TypeError as e:
     print(e)  # must be str, not NoneType
-
-
-s = 'Cat and Cat'
-index = s.find('Cat', 3)
-print(index)  # 8
