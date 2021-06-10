dcc171db8015b55bcd32e8aace2da8376fc0138b
narupo <narupo.glab@gmail.com>
2021-06-10 11:59:42 +0900

    update models

--- django/createview/sample/core/models.py
+++ django/createview/sample/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=32, help_text='投稿者名')
+    name = models.CharField(max_length=32, help_text='投稿者名', validators=[MinLengthValidator(3, '3文字以上です')])
     content = models.TextField(max_length=1024, help_text='投稿内容')
