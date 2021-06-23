d3977486f8fa11fe81e0785654d9943dfea11ed6
narupo <narupo.glab@gmail.com>
2021-06-23 21:45:33 +0900

    ok

--- python/django/cookie/mysite/mysite/urls.py
+++ python/django/cookie/mysite/mysite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home_view
+from core.views import home_view, simple_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('s/', simple_view),
 ]
 
