848eba4d60f9152cbb9552c46578be26bff7129e
narupo <narupo.glab@gmail.com>
2021-08-30 06:25:16 +0900

    add c/funcargs

---
+++ c/funcargs/funcs.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void func0(void) {
+    // ここに色々な処理
+}
+
+void func1(int arg1) {
+    // ここに色々な処理
+}
+
+void func2(int arg1, float arg2) {
+    // ここに色々な処理
+}
+
+void funcstr(const char *str) {
+    // ここに色々な処理
+}
+
+void funcarray(int ary[]) {
+    // ここに色々な処理
+}
+
+void funcptr(int *ptr) {
+    // ここに色々な処理
+}
+
+void print(int length, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+}
+
+int main(void) {
+    func0();
+    func1(123);
+    func2(123, 3.21);
+    funcstr("Hello, World!");
+
+    int ary[] = {1, 2, 3};
+    funcarray(ary);
+
+    int i = 0;
+    funcptr(&i);
+
+    print("Hello, %s!\n", "World");
+    return 0;
+}
