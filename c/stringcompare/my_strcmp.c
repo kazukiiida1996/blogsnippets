c2a7e9239615296af621a282e8981f9dc6c28170
narupo <narupo.glab@gmail.com>
2021-11-27 23:35:58 +0900

    fix bugs

--- c/stringcompare/my_strcmp.c
+++ c/stringcompare/my_strcmp.c
@@ -7,6 +7,15 @@
  * LICENSE: MIT
  */
 int my_strcmp(const char *s1, const char *s2) {
+    size_t s1len = strlen(s1);
+    size_t s2len = strlen(s2);
+
+    if (s1len > s2len) {
+        return 1;
+    } else if (s1len < s2len) {
+        return -1;
+    }
+
     for (; *s1 && *s2; s1 += 1, s2 += 1) {
         int dif = *s1 - *s2;
         if (dif < 0) {
@@ -23,5 +32,7 @@ int main(void) {
     assert(my_strcmp("Hello", "World") == strcmp("Hello", "World"));
     assert(my_strcmp("World", "Hello") == strcmp("World", "Hello"));
     assert(my_strcmp("Hige", "Hoge") == strcmp("Hige", "Hoge"));
+    assert(my_strcmp("Hige", "Hi") == strcmp("Hige", "Hi"));
+    assert(my_strcmp("Hi", "Hige") == strcmp("Hi", "Hige"));
     return 0;
 }
